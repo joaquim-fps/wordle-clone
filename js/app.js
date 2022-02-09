4bf94a7c671a039f0e978160f0a1f3d0c72eec99
joaquim-fps <joaquim.fps95@gmail.com>
2022-02-09 11:56:52 -0300

    Created words.json to reduce load times

--- js/app.js
+++ js/app.js
@@ -1,227 +1,235 @@
-import { getDictionary } from "./dictionaryFilter.js";
+(async () => {
+    let words = await getWords(),
+        right_word = "",
+        current_attempt = 0,
+        current_letter = 0,
+        game_over = false,
+        notification = document.querySelector(".notification"),
+        game_board = document.querySelectorAll(".attempt");
 
-let words = await getDictionary(),
-    right_word = "",
-    current_attempt = 0,
-    current_letter = 0,
-    game_over = false,
-    notification = document.querySelector(".notification"),
-    game_board = document.querySelectorAll(".attempt");
+    async function getWords() {
+        const wordList = await fetch("../resources/words.json").then(
+            (response) => response.json()
+        );
+        return wordList.words;
+    }
 
-function setUpKeyboard() {
-    const keyboard_letters = document.querySelectorAll(".key");
+    function setUpKeyboard() {
+        const keyboard_letters = document.querySelectorAll(".key");
 
-    keyboard_letters.forEach((letter) => {
-        letter.addEventListener("click", (e) => {
+        keyboard_letters.forEach((letter) => {
+            letter.addEventListener("click", (e) => {
+                if (!game_over) {
+                    processKey(letter.getAttribute("data-key"));
+                }
+            });
+        });
+
+        document.body.addEventListener("keydown", (e) => {
+            const re = /^[A-Za-z]{0,1}$/;
+
             if (!game_over) {
-                processKey(letter.getAttribute("data-key"));
+                switch (e.key) {
+                    case "Enter":
+                        processKey("↵");
+                        break;
+                    case "Backspace":
+                        processKey("←");
+                        break;
+                    default:
+                        if (re.test(e.key)) {
+                            processKey(e.key);
+                        }
+                }
             }
         });
-    });
+    }
 
-    document.body.addEventListener("keydown", (e) => {
-        const re = /^[A-Za-z]{0,1}$/;
+    function processKey(key) {
+        let attempt = game_board[current_attempt],
+            letter_blocks = attempt.children;
 
-        if (!game_over) {
-            switch (e.key) {
-                case "Enter":
-                    processKey("↵");
-                    break;
-                case "Backspace":
-                    processKey("←");
-                    break;
-                default:
-                    if (re.test(e.key)) {
-                        processKey(e.key);
-                    }
-            }
-        }
-    });
-}
+        const num_letter_blocks = letter_blocks.length;
 
-function processKey(key) {
-    let attempt = game_board[current_attempt],
-        letter_blocks = attempt.children;
+        notification.classList = "notification hide";
 
-    const num_letter_blocks = letter_blocks.length;
+        switch (key) {
+            case "↵" || "Enter":
+                if (letter_blocks[num_letter_blocks - 1].textContent != "") {
+                    if (checkValidity(attempt)) {
+                        attempt.classList.remove("current-attempt");
 
-    notification.classList = "notification hide";
-
-    switch (key) {
-        case "↵" || "Enter":
-            if (letter_blocks[num_letter_blocks - 1].textContent != "") {
-                if (checkValidity(attempt)) {
-                    attempt.classList.remove("current-attempt");
-
-                    if (checkAttempt(attempt) == 5) {
-                        notification.textContent = "Congratulations!";
-                        notification.classList.remove("hide");
-                        notification.classList.add("show", "won");
-                        game_over = true;
-                    } else {
-                        current_letter = 0;
-
-                        if (current_attempt < 5) {
-                            current_attempt++;
-                            attempt =
-                                document.querySelectorAll(".attempt")[
-                                    current_attempt
-                                ];
-                            attempt.classList.add("current-attempt");
-                        } else {
-                            notification.textContent =
-                                "The right word was " +
-                                right_word.toUpperCase();
+                        if (checkAttempt(attempt) == 5) {
+                            notification.textContent = "Congratulations!";
                             notification.classList.remove("hide");
-                            notification.classList.add("show", "lost");
+                            notification.classList.add("show", "won");
                             game_over = true;
+                        } else {
+                            current_letter = 0;
+
+                            if (current_attempt < 5) {
+                                current_attempt++;
+                                attempt =
+                                    document.querySelectorAll(".attempt")[
+                                        current_attempt
+                                    ];
+                                attempt.classList.add("current-attempt");
+                            } else {
+                                notification.textContent =
+                                    "The right word was " +
+                                    right_word.toUpperCase();
+                                notification.classList.remove("hide");
+                                notification.classList.add("show", "lost");
+                                game_over = true;
+                            }
                         }
+                    } else {
+                        notification.textContent = "Invalid word!";
+                        notification.classList.remove("hide");
+                        notification.classList.add("show", "invalid");
                     }
                 } else {
-                    notification.textContent = "Invalid word!";
+                    notification.textContent = "Not enough letters!";
                     notification.classList.remove("hide");
-                    notification.classList.add("show", "invalid");
+                    notification.classList.add("show", "notenough");
                 }
-            } else {
-                notification.textContent = "Not enough letters!";
-                notification.classList.remove("hide");
-                notification.classList.add("show", "notenough");
-            }
-            break;
-        case "←" || "Backspace":
-            if (current_letter > 0) current_letter--;
-            letter_blocks[current_letter].textContent = "";
-            break;
-        default:
-            if (current_letter < num_letter_blocks) {
-                letter_blocks[current_letter].textContent = key.toLowerCase();
-                current_letter++;
-            }
-    }
-}
-
-function checkValidity(attempt) {
-    let attempt_letters = Array.from(attempt.children),
-        word_guess = [
-            ...attempt_letters.map((letter) => letter.textContent),
-        ].join("");
-
-    return words.includes(word_guess);
-}
-
-function checkAttempt(attempt) {
-    let attempt_letters = Array.from(attempt.children),
-        right_letters = right_word.split(""),
-        letter_count = letterCount(right_word),
-        num_right_guesses = 0;
-
-    for (let i in attempt_letters) {
-        let letter = attempt_letters[i];
-        letter.classList.add("flip-horizontal-bottom");
-
-        if (letter.textContent == right_word[i]) {
-            letter.classList.add("correct");
-            document
-                .querySelector("#" + letter.textContent)
-                .classList.add("correct");
-            letter_count[letter.textContent]--;
-            num_right_guesses++;
+                break;
+            case "←" || "Backspace":
+                if (current_letter > 0) current_letter--;
+                letter_blocks[current_letter].textContent = "";
+                break;
+            default:
+                if (current_letter < num_letter_blocks) {
+                    letter_blocks[current_letter].textContent =
+                        key.toLowerCase();
+                    current_letter++;
+                }
         }
     }
 
-    for (let i in attempt_letters) {
-        let letter = attempt_letters[i];
+    function checkValidity(attempt) {
+        let attempt_letters = Array.from(attempt.children),
+            word_guess = [
+                ...attempt_letters.map((letter) => letter.textContent),
+            ].join("");
 
-        if (!letter.classList.contains("correct")) {
-            if (
-                right_letters.includes(letter.textContent) &&
-                letter_count[letter.textContent] > 0
-            ) {
-                letter.classList.add("misplaced");
+        return words.includes(word_guess);
+    }
+
+    function checkAttempt(attempt) {
+        let attempt_letters = Array.from(attempt.children),
+            right_letters = right_word.split(""),
+            letter_count = letterCount(right_word),
+            num_right_guesses = 0;
+
+        for (let i in attempt_letters) {
+            let letter = attempt_letters[i];
+            letter.classList.add("flip-horizontal-bottom");
+
+            if (letter.textContent == right_word[i]) {
+                letter.classList.add("correct");
                 document
                     .querySelector("#" + letter.textContent)
-                    .classList.add("misplaced");
+                    .classList.add("correct");
                 letter_count[letter.textContent]--;
-            } else {
-                letter.classList.add("absent");
-                document
-                    .querySelector("#" + letter.textContent)
-                    .classList.add("absent");
+                num_right_guesses++;
             }
         }
+
+        for (let i in attempt_letters) {
+            let letter = attempt_letters[i];
+
+            if (!letter.classList.contains("correct")) {
+                if (
+                    right_letters.includes(letter.textContent) &&
+                    letter_count[letter.textContent] > 0
+                ) {
+                    letter.classList.add("misplaced");
+                    document
+                        .querySelector("#" + letter.textContent)
+                        .classList.add("misplaced");
+                    letter_count[letter.textContent]--;
+                } else {
+                    letter.classList.add("absent");
+                    document
+                        .querySelector("#" + letter.textContent)
+                        .classList.add("absent");
+                }
+            }
+        }
+
+        return num_right_guesses;
     }
 
-    return num_right_guesses;
-}
+    function letterCount(word) {
+        let letters = word.split(""),
+            uniqueLetters = [...new Set(letters)],
+            letterCount = {};
 
-function letterCount(word) {
-    let letters = word.split(""),
-        uniqueLetters = [...new Set(letters)],
-        letterCount = {};
-
-    uniqueLetters.forEach((letter) => {
-        letterCount[letter] = word.split(letter).length - 1;
-    });
-
-    return letterCount;
-}
-
-function shuffle(array) {
-    let currentIndex = array.length,
-        randomIndex;
-
-    // While there remain elements to shuffle...
-    while (currentIndex != 0) {
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-
-        // And swap it with the current element.
-        [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex],
-            array[currentIndex],
-        ];
-    }
-
-    return array;
-}
-
-function setNewGame() {
-    right_word = shuffle(words)[42];
-    console.log(right_word);
-
-    game_board.forEach(function (attempt) {
-        attempt.classList = "attempt";
-    });
-
-    game_board[0].classList.add("current-attempt");
-
-    document
-        .querySelectorAll(".game .letter-block")
-        .forEach(function (letter_block) {
-            letter_block.classList = "letter-block";
-            letter_block.textContent = "";
+        uniqueLetters.forEach((letter) => {
+            letterCount[letter] = word.split(letter).length - 1;
         });
 
-    document.querySelectorAll(".key").forEach(function (key) {
-        key.classList = "key";
-    });
+        return letterCount;
+    }
 
-    document.querySelector(".notification").classList = "notification hide";
+    function shuffle(array) {
+        let currentIndex = array.length,
+            randomIndex;
 
-    current_attempt = 0;
-    current_letter = 0;
+        // While there remain elements to shuffle...
+        while (currentIndex != 0) {
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
 
-    game_over = false;
-}
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex],
+                array[currentIndex],
+            ];
+        }
 
-function app() {
-    document
-        .querySelector("#reload_button")
-        .addEventListener("click", setNewGame);
+        return array;
+    }
 
-    setUpKeyboard();
-    setNewGame();
-}
+    function setNewGame() {
+        right_word = shuffle(words)[42];
+        console.log(right_word);
 
-app();
+        game_board.forEach(function (attempt) {
+            attempt.classList = "attempt";
+        });
+
+        game_board[0].classList.add("current-attempt");
+
+        document
+            .querySelectorAll(".game .letter-block")
+            .forEach(function (letter_block) {
+                letter_block.classList = "letter-block";
+                letter_block.textContent = "";
+            });
+
+        document.querySelectorAll(".key").forEach(function (key) {
+            key.classList = "key";
+        });
+
+        document.querySelector(".notification").classList = "notification hide";
+
+        current_attempt = 0;
+        current_letter = 0;
+
+        game_over = false;
+    }
+
+    function app() {
+        document
+            .querySelector("#reload_button")
+            .addEventListener("click", setNewGame);
+
+        setUpKeyboard();
+        setNewGame();
+    }
+
+    app();
+})();
--- js/dictionaryFilter.js
+++ js/dictionaryFilter.js
@@ -1,9 +1,22 @@
-export async function getDictionary() {
-    let dictionary = await fetch('./resources/dictionary.json').then(response => response.json());
+const fs = require("fs");
+const path = require("path");
+const dictionary = require("../resources/dictionary.json");
+
+async function getDictionary() {
     let words = Object.keys(dictionary);
-    words = words.filter((word) => word.length == 5 && word[0] != '-' && word[4] != '-');
-    return [... new Set(words.map((wordObj) => wordObj.toLowerCase()))];
+    words = words.filter(
+        (word) => word.length == 5 && word[0] != "-" && word[4] != "-"
+    );
+    return [...new Set(words.map((wordObj) => wordObj.toLowerCase()))];
 }
 
-
-
+getDictionary().then((wordList) => {
+    wordList = { words: wordList };
+    fs.writeFile(
+        path.resolve(__dirname, "../resources/words.json"),
+        JSON.stringify(wordList),
+        (err) => {
+            if (err) throw err;
+        }
+    );
+});
